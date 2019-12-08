c20ddb81c585f177b0982b7b493c18b67ab847fd
unknown <darques@cifpfbmoll.eu>
2019-12-08 18:35:07 +0100

    refactor:Modificar smallStraight

--- yatzy.py
+++ yatzy.py
@@ -43,20 +43,15 @@ class Yatzy:
             return 0
 
     @staticmethod
-    def smallStraight(d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[0] == 1 and
-            tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-                tallies[4] == 1):
+    def smallStraight(*dice):
+        isStraight = True
+        for number in range(1, 5):
+            if number not in dice:
+                isStraight = False
+        if isStraight is True:
             return 15
-        return 0
+        else:
+            return 0
 
     @staticmethod
     def largeStraight(d1,  d2,  d3,  d4,  d5):
