5a26763fe180cacdb4638f1bad3ec5b9d86761f1
unknown <darques@cifpfbmoll.eu>
2019-12-08 18:22:29 +0100

    refactor:Reorganizar orden tests

--- test_yatzy.py
+++ test_yatzy.py
@@ -61,12 +61,6 @@ def test_one_pair():
     assert 12 == Yatzy.amountOfAKind(2, 5, 3, 6, 6, 6)
 
 
-def test_two_Pair():
-    assert 16 == Yatzy.two_pair(3, 3, 5, 4, 5)
-    assert 18 == Yatzy.two_pair(3, 3, 6, 6, 6)
-    assert 0 == Yatzy.two_pair(3, 3, 6, 5, 4)
-
-
 def test_three_of_a_kind():
     assert 9 == Yatzy.amountOfAKind(3, 3, 3, 3, 4, 5)
     assert 15 == Yatzy.amountOfAKind(3, 5, 3, 5, 4, 5)
@@ -78,6 +72,11 @@ def test_four_of_a_knd():
     assert 20 == Yatzy.amountOfAKind(4, 5, 5, 5, 4, 5)
     assert 12 == Yatzy.amountOfAKind(4, 3, 3, 3, 3, 3)
     assert 0 == Yatzy.amountOfAKind(4, 3, 3, 3, 2, 1)
+
+def test_two_Pair():
+    assert 16 == Yatzy.two_pair(3, 3, 5, 4, 5)
+    assert 18 == Yatzy.two_pair(3, 3, 6, 6, 6)
+    assert 0 == Yatzy.two_pair(3, 3, 6, 5, 4)
 
 
 def test_smallStraight():
