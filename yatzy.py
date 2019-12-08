df07df27d94d62cadbb9e594acc79a6a630a0cb3
unknown <darques@cifpfbmoll.eu>
2019-12-08 17:41:25 +0100

    refactor:Eliminar funciones twos, threes... innecesarias

--- yatzy.py
+++ yatzy.py
@@ -21,36 +21,6 @@ class Yatzy:
                 sum += dieCategory
         return sum
 
-    @staticmethod
-    def twos(d1,  d2,  d3,  d4,  d5):
-        sum = 0
-        if (d1 == 2):
-            sum += 2
-        if (d2 == 2):
-            sum += 2
-        if (d3 == 2):
-            sum += 2
-        if (d4 == 2):
-            sum += 2
-        if (d5 == 2):
-            sum += 2
-        return sum
-
-    @staticmethod
-    def threes(d1,  d2,  d3,  d4,  d5):
-        s = 0
-        if (d1 == 3):
-            s += 3
-        if (d2 == 3):
-            s += 3
-        if (d3 == 3):
-            s += 3
-        if (d4 == 3):
-            s += 3
-        if (d5 == 3):
-            s += 3
-        return s
-
     def __init__(self, d1, d2, d3, d4, _5):
         self.dice = [0]*5
         self.dice[0] = d1
@@ -58,28 +28,6 @@ class Yatzy:
         self.dice[2] = d3
         self.dice[3] = d4
         self.dice[4] = _5
-
-    def fours(self):
-        sum = 0
-        for at in range(5):
-            if (self.dice[at] == 4):
-                sum += 4
-        return sum
-
-    def fives(self):
-        s = 0
-        i = 0
-        for i in range(len(self.dice)):
-            if (self.dice[i] == 5):
-                s = s + 5
-        return s
-
-    def sixes(self):
-        sum = 0
-        for at in range(len(self.dice)):
-            if (self.dice[at] == 6):
-                sum = sum + 6
-        return sum
 
     @staticmethod
     def score_pair(d1,  d2,  d3,  d4,  d5):
